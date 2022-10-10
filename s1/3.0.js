6206590b7f858eb00f50cd9d222c2f9f503121be
IMG-2017\Stud <stud@ase.ro>
2022-10-10 11:04:25 +0300

    seminar2

--- s1/3.0.js
+++ s1/3.0.js
@@ -7,3 +7,10 @@ const countAppearences = (s,c) => {
     }
     return count;
 }
+
+console.log(countAppearences('a little cat', 'a'))
+console.log(countAppearences('','a'))
+
+const countAppearences2 = (s,c) => s.split(c).length
+console.log(countAppearences('a little cat', 'a'))
+console.log(countAppearences('','a'))
